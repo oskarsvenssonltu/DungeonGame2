10533d6b8a779232aa9756030371a2968b2959b5
EmeliLy <emelie.lysell@hotmail.com>
2025-12-10 08:16:24 +0100

    Kommentarer klara

--- src/dungeongame2/Player.java
+++ src/dungeongame2/Player.java
@@ -1,22 +1,30 @@
 package dungeongame2;
 
 public class Player {
+
+    // Spelarens namn.
     private String name;
+
+    // Rummet som spelaren befinner sig i nu.
     private Room currentRoom;
 
+    // Skapar en spelare med namn och startrum.
     public Player(String name, Room startRoom) {
         this.name = name;
         this.currentRoom = startRoom;
     }
     
-    public String getName(){
+    // Hämtar spelarens namn. 
+    public String getName() {
         return name;
     }
 
+    // Hämtar rummet som spelaren befinner sig i.
     public Room getCurrentRoom() {
         return currentRoom;
     }
 
+    // Flyttar spelaren till nästa rum.
     public void moveTo(Room room) {
         this.currentRoom = room;
     }

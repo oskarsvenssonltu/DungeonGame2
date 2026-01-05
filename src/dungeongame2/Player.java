de4c510b3543ae8283362e51db9a2b045174379f
EmeliLy <emelie.lysell@hotmail.com>
2026-01-05 14:23:19 +0100

    upd spelare

--- src/dungeongame2/Player.java
+++ src/dungeongame2/Player.java
@@ -1,31 +1,118 @@
 package dungeongame2;
 
+import java.util.ArrayList;
+
+/*
+ * Klass för spelaren.
+ * Spelaren har namn, hälsopoäng, skada, väska (inv), 
+   currentRoom.
+ */
 public class Player {
 
-    // Spelarens namn.
+    // Spelarens namn
     private String name;
 
-    // Rummet som spelaren befinner sig i nu.
+    // Spelarens hälsa (starta på 10)
+    private int healthPoints;
+
+    // Spelarens grundskada ( 1 hälsopoäng)
+    private int damage;
+
+    // Rummet som spelaren befinner sig i nu
     private Room currentRoom;
 
-    // Skapar en spelare med namn och startrum.
+    // Spelarens inventory (väska)
+    private ArrayList<Item> inventory = new ArrayList<>();
+
+    /*
+     * Konstruktor som skapar spelare med namn och startrum.
+     * standardvärden för HP och damage.
+     */
     public Player(String name, Room startRoom) {
         this.name = name;
         this.currentRoom = startRoom;
+        this.healthPoints = 10;
+        this.damage = 1;
     }
-    
-    // Hämtar spelarens namn. 
+
+    //Getters
+
     public String getName() {
         return name;
     }
 
-    // Hämtar rummet som spelaren befinner sig i.
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
     public Room getCurrentRoom() {
         return currentRoom;
     }
 
-    // Flyttar spelaren till nästa rum.
+    //Setters 
+
+    //Flyttar spelare till nytt rum.
+     
     public void moveTo(Room room) {
         this.currentRoom = room;
+    }
+
+    // Spelare tar skada (minskar HP).
+     
+    public void takeDamage(int amount) {
+        healthPoints -= amount;
+        if (healthPoints < 0) {
+            healthPoints = 0;
+        }
+    }
+
+
+    // Returnerar true om spelaren lever.
+     
+     public boolean isAlive() {
+        return healthPoints > 0;
+    }
+
+    //Inventory
+
+    // Lägger till ett item i inventory.
+    
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+
+    // Kollar om spelaren har en nyckel i inventory.
+  
+    public boolean hasKey() {
+        for (Item item : inventory) {
+            if (item instanceof Key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Skriver ut spelarens inventory.
+     
+    public void printInventory() {
+        if (inventory.isEmpty()) {
+            System.out.println("Din väska är tom.\n");
+            return;
+        }
+
+        System.out.println("Du bär på:");
+        for (Item item : inventory) {
+            System.out.println("- " + item.getName());
+        }
+        System.out.println();
+    }
+
+    //Ökar spelarens skada.
+    public void increaseDamage(int amount) {
+        damage += amount;
     }
 }
